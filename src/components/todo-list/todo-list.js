9ebe1cec76f465b7426e14142e11465a28757bb8
Dana Vergilyush <dvergilyush@gmail.com>
2020-01-17 04:19:42 +0300

    Structure reorganization and refactoring

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import './todo-list.css'
 
 import TodoListItem from '../todo-list-item'
 
 const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
         const {id, ...itemProps} = item;
+        
         return (
-            <li key={id}>
+            <li className="list-group-item"
+                key={id}>
                 <TodoListItem {...itemProps} />
             </li>
         )
     })
 
     return (
-        <ul>
+        <ul className="list-group todo-list">
             { elements }
         </ul>
     );
