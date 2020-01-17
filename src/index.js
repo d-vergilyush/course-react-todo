82d127d55d642d58d4be483c55fb8fa6f2fecd2d
Dana Vergilyush <dvergilyush@gmail.com>
2020-01-17 04:21:56 +0300

    Add links to cdn for styles

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TodoList from './components/todo-list';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
+import App from './components/app';
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink coffee', important: false, id: '1' },
-        { label: 'Learn React', important: false, id: '2' },
-        { label: 'Сorrespond in a telegram', important: false, id: '3' },
-        { label: 'Create awesome app', important: true, id: '4' }
-    ]    
+ReactDOM.render(<App />,
+  document.getElementById('root'));
 
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos={todoData} />
-        </div>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
